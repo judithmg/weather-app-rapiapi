bd599083fec2e4bb5fd5f2664a2ab82f42f3e0e5
judithmg <jmartigo33@gmail.com>
2022-06-02 10:52:24 +0200

    add City type

---
+++ src/types/weather.ts
@@ -0,0 +1,142 @@
+export interface Weather {
+  location: {
+    name: string;
+    region: string;
+    country: string;
+    lat: number;
+    lon: number;
+    tz_id: string;
+    localtime_epoch: number;
+    localtime: string;
+  };
+  current: {
+    last_updated_epoch: number;
+    last_updated: string;
+    temp_c: number;
+    temp_f: number;
+    is_day: number;
+    condition: {
+      text: string;
+      icon: string;
+      code: number;
+    };
+    wind_mph: number;
+    wind_kph: number;
+    wind_degree: number;
+    wind_dir: string;
+    pressure_mb: number;
+    pressure_in: number;
+    precip_mm: number;
+    precip_in: number;
+    humidity: number;
+    cloud: number;
+    feelslike_c: number;
+    feelslike_f: number;
+    vis_km: number;
+    vis_miles: number;
+    uv: number;
+    gust_mph: number;
+    gust_kph: number;
+  };
+  forecast: {
+    forecastday: ForecastDay[];
+  };
+}
+
+export interface ForecastDay {
+  date: string;
+  date_epoch: Date;
+  day: Day;
+  astro: Astro;
+  hour: Hour[];
+}
+
+interface Day {
+  maxtemp_c: number;
+  maxtemp_f: number;
+  mintemp_c: number;
+  mintemp_f: number;
+  avgtemp_c: number;
+  avgtemp_f: number;
+  maxwind_mph: number;
+  maxwind_kph: number;
+  totalprecip_mm: number;
+  totalprecip_in: number;
+  avgvis_km: number;
+  avgvis_miles: number;
+  avghumidity: number;
+  daily_will_it_rain: number;
+  daily_chance_of_rain: number;
+  daily_will_it_snow: number;
+  daily_chance_of_snow: number;
+  condition: {
+    code: number;
+    icon: string;
+    text: string;
+  };
+  uv: number;
+}
+
+interface Astro {
+  sunrise: string;
+  sunset: string;
+  moonrise: string;
+  moonset: string;
+  moon_phase: string;
+  moon_illumination: string;
+}
+
+export interface Hour {
+  chance_of_rain: number;
+  chance_of_snow: number;
+  cloud: number;
+  condition: {
+    code: number;
+    icon: string;
+    text: string;
+  };
+  dewpoint_c: number;
+  dewpoint_f: number;
+  feelslike_c: number;
+  feelslike_f: number;
+  gust_kph: number;
+  gust_mph: number;
+  heatindex_c: number;
+  heatindex_f: number;
+  humidity: number;
+  is_day: number;
+  precip_in: number;
+  precip_mm: number;
+  pressure_in: number;
+  pressure_mb: number;
+  temp_c: number;
+  temp_f: number;
+  time_epoch: number;
+  time: string;
+  uv: number;
+  vis_km: number;
+  vis_miles: number;
+  will_it_rain: number;
+  will_it_snow: number;
+  wind_degree: number;
+  wind_dir: string;
+  wind_kph: number;
+  wind_mph: number;
+  windchill_c: number;
+  windchill_f: number;
+}
+
+export interface ChartData {
+  labels: number[];
+  data: number[];
+}
+
+export interface City {
+  id: number;
+  name: string;
+  country: string;
+  region: string;
+  url: string;
+  lat: number;
+  lon: number;
+}
